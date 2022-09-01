0dd09b024cf0dab1dee0c6cd71c036cba4a09b25
running88 <7047149+running88@users.noreply.github.com>
2022-09-01 12:59:50 +0800

    Create picklistform.js

---
+++ picklistform.js
@@ -0,0 +1,15 @@
+$( function() {
+    $( "#00N5g00000TCe8G, #00N5g00000TCe8L" ).datepicker({ 
+		changeMonth: true,
+		changeYear: true,
+		yearRange: "c-100:c+0", 
+		dateFormat: 'mm/dd/yy' });
+  } );
+  
+  $( function() {
+    $( "#00N5g00000TCe8LXTWO" ).datepicker({ 
+		changeMonth: true,
+		changeYear: true,
+		yearRange: "c-100:c+0", 
+		dateFormat: 'mm/dd/yy' });
+  } );
